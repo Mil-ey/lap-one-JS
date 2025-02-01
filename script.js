d0c676a4e7bada77f30b790880f38a5a96e06cf6
Mil-ey <mebhiryti7798@gmail.com>
2025-02-01 00:11:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let studentName = "Milli";
+console.log(studentName);
+let studentAge = 24;
+console.log(studentAge);
+let isEnrolled = true;
+console.log(isEnrolled);
+
+
+let text = "Hello, Guys!";
+console.log(text);
+
+
+let studentUniform = ["white shirt", "red tie", "black pants", "black shoes"];
+console.log(studentUniform);
+
+let student = {
+    name: "Milli",
+    age: 24,
+    isEnrolled: true,
+};
+console.log(student);
+
+
+console.log(54/3);
+console.log(54*3);
+console.log(54+3);
+console.log(54-3);
+console.log(54%3);
+
+
+let x = 54;
+let y = 3;
+console.log(x > y);
+console.log(x < y);
+console.log(x == y);
+
+
+let age = 27;
+let isEnrolled = true;
+console.log(age > 20 && isEnrolled);
+console.log(age > 20 || isEnrolled);
